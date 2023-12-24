63533f71d90eb1eac294568c525972f69f5e8636
pashkatrick <coolbatmanarrow@gmail.com>
2023-12-24 21:03:01 +0300

    [master] pypi removed

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 
 
 setup(
-    name='soft-asserts',
-    version='1.0.2',
+    name='soft-assert',
+    version='1.0.0',
     description='Soft assertions for Python/Pytest',
     long_description=io.open(os.path.join(os.path.dirname('__file__'), 'README.md'), encoding='utf-8').read(),
     long_description_content_type='text/markdown',
