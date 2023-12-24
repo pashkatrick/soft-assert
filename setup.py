468bc872cc34ccf221f30c494548d7252162e2e8
pashkatrick <coolbatmanarrow@gmail.com>
2023-12-24 20:34:06 +0300

    [upgrd] link

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ from setuptools import setup
 
 setup(
     name='soft-asserts',
-    version='1.0.1',
+    version='1.0.2',
     description='Soft assertions for Python/Pytest',
     long_description=io.open(os.path.join(os.path.dirname('__file__'), 'README.md'), encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     author='pashkatrick',
-    url='https://github.com/balalaiQA/soft-asserts',
+    url='https://github.com/pashkatrick/soft-asserts',
     packages=['soft_asserts']
 )
