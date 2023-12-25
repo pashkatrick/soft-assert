802731e35e4899fb3bd2b853aa1475df70497d02
Pavel Yudin <coolbatmanarrow@gmail.com>
2023-12-25 14:39:16 +0300

    [issue-2] add funcs (#4)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='soft-assert',
-    version='1.0.1',
+    version='1.0.2',
     description='Soft assertions for Python/Pytest',
     long_description=io.open(os.path.join(os.path.dirname('__file__'), 'README.md'), encoding='utf-8').read(),
     long_description_content_type='text/markdown',
